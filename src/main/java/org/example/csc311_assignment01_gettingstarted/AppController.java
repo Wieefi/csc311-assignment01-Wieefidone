14caf4c8368f22f0dbddc03b382ce287a1c9a49e
Efrat Weiss <weise24@farmingdale.edu>
2025-01-31 22:27:18 -0500

    Created labels, buttons, and implemented input validation for numeric vals (as well as calculation.)

--- src/main/java/org/example/csc311_assignment01_gettingstarted/AppController.java
+++ src/main/java/org/example/csc311_assignment01_gettingstarted/AppController.java
@@ -1,7 +1,9 @@
 package org.example.csc311_assignment01_gettingstarted;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class AppController {
     @FXML
@@ -9,17 +11,54 @@ public class AppController {
 
     // ToDo 01: add two text fields to the GUI so that the user can find the sum
 
+    @FXML
+    private TextField numText1;
+    @FXML
+    private TextField numText2;
+
+
     // ToDo 02: when the button is clicked show the output on the screen
 
-    // ToDo 03: you should accept only the numeric values
+    @FXML
+    private Button calculateButton;
+    @FXML
+    private Label resultLabel;
+
+    @FXML
+    protected void onCalculateButtonClick() {
+        // ToDo 03: you should accept only the numeric values
+        if (!isNumeric(numText1.getText()) || !isNumeric(numText2.getText())){
+            resultLabel.setText("Only enter numeric values please :)");
+            return;
+        }
+        double num1 = Double.parseDouble(numText1.getText());
+        double num2 = Double.parseDouble(numText2.getText());
+        double sum = num1 + num2;
+        resultLabel.setText("Sum = " + sum);
+    }
+
+    private boolean isNumeric(String str){
+        if (str ==null || str.trim().isEmpty()){
+            return false;
+        }
+        try{
+            Double.parseDouble(str);
+            return true;
+        }catch (NumberFormatException e){
+            return false;
+        }
+
+
+
+    }
+
+
+
+    }
+
+
 
     // ToDo 04: modify the CSS file to make the background of the application Lightblue and the button color red
 
     // ToDo 05: commit changes and push back to the same repo
 
-
-    @FXML
-    protected void onCalculateButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
-}
--- src/main/java/org/example/csc311_assignment01_gettingstarted/MainApplication.java
+++ src/main/java/org/example/csc311_assignment01_gettingstarted/MainApplication.java
@@ -10,10 +10,11 @@ import java.io.IOException;
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("app-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/org/example/csc311_assignment01_gettingstarted/app-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        scene.getStylesheets().add(MainApplication.class.getResource("appstyle.css").toExternalForm());
-        stage.setTitle("Hello!");
+        scene.getStylesheets().add(getClass().getResource("/org/example/csc311_assignment01_gettingstarted/appstyle.css").toExternalForm());
+
+        stage.setTitle("JavaFX App");
         stage.setScene(scene);
         stage.show();
     }
